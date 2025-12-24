bc817456f5a1f1aa0be21f7a3e489125f53a711c
Eldor Asher <eldorasher@MacBook-Air-sl-Eldor.local>
2025-12-24 07:10:00 +0200

    done

--- Palindrome.java
+++ Palindrome.java
@@ -2,12 +2,13 @@
 public class Palindrome {
 
 	public static void main(String[]args) {
-    	System.out.println(isPalindrome(args[0]));
+    	System.out.println(isPalindrome("kayak"));
     }
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+		if (s.length() <2) return true;
+		else if (s.charAt(0) != s.charAt(s.length() -1)) return false;
+		return (isPalindrome(s.substring(1, s.length() - 1)));
     }
 }
