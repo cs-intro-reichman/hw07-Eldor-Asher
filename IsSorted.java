4cda6e887b4e95f77608bef85ea763ace13865b7
Eldor Asher <eldorasher@MacBook-Air-sl-Eldor.local>
2025-12-24 12:42:20 +0200

    done

--- IsSorted.java
+++ IsSorted.java
@@ -7,8 +7,10 @@ public class IsSorted {
 
     // Helper recursive function
     private static boolean isSorted(int[] a, int i) {
-        //// Replace the following statement with your code
-        return false; 
+        if (i == a.length) return true;
+        else if (a[i] == a[i+1]) return true;
+        else if (a[i] > a[i+1]) return false;
+        return isSorted(a, i+1);
     }
 
     public static void main(String[] args) {
